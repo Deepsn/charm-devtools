61540b04561441ff021a867e9a8dc50feefa793f
Deepsn <75620590+Deepsn@users.noreply.github.com>
2026-07-20 21:07:46 -0300

    refactor: move dev detection constants to core and simplify plugin entry

--- plugin/src/constants/core.ts
+++ plugin/src/constants/core.ts
@@ -1,3 +1,5 @@
-import { RunService } from "@rbxts/services";
+import { Players, RunService } from "@rbxts/services";
 
 export const IS_RUNNING = RunService.IsRunning();
+export const IS_A_DEV = Players.LocalPlayer?.UserId === 78662128;
+export const IS_DEV = (plugin: Plugin) => game.PlaceId === 0 && plugin.Name.match("user_")[0] !== undefined;
--- plugin/src/plugin.server.ts
+++ plugin/src/plugin.server.ts
@@ -6,56 +6,52 @@ import { mount } from "@rbxts/vide";
 import { App } from "app";
 import { addToHistory } from "atoms/history";
 import { enabled } from "atoms/plugin";
-import { IS_RUNNING } from "constants/core";
+import { IS_A_DEV, IS_RUNNING } from "constants/core";
 
-const IS_DEV = game.PlaceId === 0 && plugin.Name.match("user_")[0] !== undefined;
+const toolbar = plugin.CreateToolbar("Charm DevTools");
+const button = toolbar.CreateButton("Open devtools", "", "");
 
-function main() {
-	const toolbar = plugin.CreateToolbar("Charm DevTools");
-	const button = toolbar.CreateButton("Open devtools", "", "");
+const widget = plugin.CreateDockWidgetPluginGuiAsync(
+	"charm-devtools",
+	new DockWidgetPluginGuiInfo(Enum.InitialDockState.Float, false, false, 600, 400),
+);
+widget.Name = "Charm DevTools";
+widget.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
 
-	const widget = plugin.CreateDockWidgetPluginGuiAsync(
-		"charm-devtools",
-		new DockWidgetPluginGuiInfo(Enum.InitialDockState.Float, false, false, 600, 400),
-	);
-	widget.Name = "Charm DevTools";
-	widget.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+const bridge = IS_RUNNING ? createBridge(addToHistory) : undefined;
 
-	const bridge = IS_RUNNING ? createBridge(addToHistory) : undefined;
+let unmount: (() => void) | undefined;
 
-	let unmount: (() => void) | undefined;
+button.Click.Connect(() => enabled((prev) => !prev));
+(widget as unknown as { BindToClose(callback: () => void): void }).BindToClose(() => enabled(false));
 
-	button.Click.Connect(() => enabled((prev) => !prev));
-	(widget as unknown as { BindToClose(callback: () => void): void }).BindToClose(() => enabled(false));
+const disposeEnabled = listen(enabled, (isEnabled) => {
+	widget.Enabled = isEnabled;
+	button.SetActive(isEnabled);
 
-	const disposeEnabled = listen(enabled, (isEnabled) => {
-		widget.Enabled = isEnabled;
-		button.SetActive(isEnabled);
-
-		if (isEnabled) {
-			unmount ??= mount(() => App(), widget);
-		} else {
-			unmount?.();
-			unmount = undefined;
-		}
-	});
-
-	if (IS_DEV && IS_RUNNING) {
-		enabled(true);
-	}
-
-	plugin.Unloading.Connect(() => {
-		enabled(false);
-		trigger(enabled);
-
-		bridge?.dispose();
-		disposeEnabled();
-
+	if (isEnabled) {
+		unmount ??= mount(() => App(), widget);
+	} else {
 		unmount?.();
-		widget.Destroy();
-	});
+		unmount = undefined;
+	}
+});
+
+if (IS_A_DEV && IS_RUNNING) {
+	enabled(true);
 }
 
-// if (IS_DEV) {
-main();
-// }
+plugin.Unloading.Connect(() => {
+	enabled(false);
+	trigger(enabled);
+
+	bridge?.dispose();
+	disposeEnabled();
+
+	unmount?.();
+	widget.Destroy();
+});
+
+if (IS_A_DEV) {
+	print("charm-devtools running");
+}
